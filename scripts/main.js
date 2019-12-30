7009247fc50b704bd8b742440548cd0895f59b6f
Megalopsychic <mfkourie@gmail.com>
2019-12-30 10:12:11 +0000

    added basic functions and barebones HTML

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+//Mathematical Operations
+function addition(a, b) {
+    a + b;
+}
+
+function subtraction(a, b) {
+    a - b;
+}
+
+function multiplication(a, b) {
+    a * b;
+}
+
+function division(a, b) {
+    a / b;
+}
+
+//Operator
+function operator(operation, a, b) {
+    operation(a, b)
+}
