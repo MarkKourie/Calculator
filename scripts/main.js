fdc454090b63a74079992f690f4f68f112adaa6d
Megalopsychic <mfkourie@gmail.com>
2020-01-04 10:18:26 +0000

    Fixed some little bugs regarding equals sign being pressed with operator as last entry.

--- scripts/main.js
+++ scripts/main.js
@@ -38,14 +38,17 @@ calculatorButtons.forEach((button) => {
     })
 });
 
-//fix so that if first letter of string cannot be operator.
-
 //Operator Buttons
 const operatorButtons = document.querySelectorAll('.operation');
 operatorButtons.forEach((button) => {
     button.addEventListener('click', (e) => {
         if (/\d$/.test(displayValue.textContent)) {
             history.textContent = history.textContent + displayValue.textContent + ` ${button.id} `;
+            displayValue.textContent = `${button.id}`;
+        } else if (displayValue.textContent == '' && (button.id == '*' || button.id == '/')) {
+            displayValue.textContent = '';
+        } else if (displayValue.textContent == '' && (button.id == "+" || button.id == "-")) {
+            history.textContent = `0 ${button.id} `;
             displayValue.textContent = `${button.id}`;
         } else {
             history.textContent = history.textContent.slice(0, -3) + ` ${button.id} `;
@@ -64,9 +67,15 @@ equalsButton.addEventListener('click', (e) => {
     displayValue.textContent = '=';
     let arithmeticString = history.textContent
     currentOperation = arithmeticString.split(' ');
+    if (/\D$/.test(history.textContent)) {
+        clearAll()
+        throw new Error("STOAHP");
+    } else {
         while (currentOperation.length !== 1) {
-        arithemeticLogic(currentOperation);
+            arithemeticLogic(currentOperation);
+        }
     }
+    //modify currentOperation[0] to fixed decimal places or scientfic notation if too large for the screen
     resultDiv.textContent = currentOperation[0];
 });
 
@@ -74,30 +83,18 @@ equalsButton.addEventListener('click', (e) => {
 const clearButton = document.querySelector('#clear');
 clearButton.addEventListener('click', (e) => clearAll());
 
-function findAdjacentValues(array, value) {
-    let left = array.indexOf(value) - 1;
-    let right = array.indexOf(value) + 1;
-    let result = [parseFloat(array[left]), parseFloat(array[right])];
-    return result;
-};
-
-function replaceOperationAndAdjacentValues(array, operation, result) {
-    let left = array.indexOf(operation) - 1;
-    array.splice(left, 3, result);
-};
 
 //Operate function
 function arithemeticLogic(array) {
     array.forEach((item) => {
-        if (item === "*") {
-            let product = multiplication(findAdjacentValues(array, item));
-            replaceOperationAndAdjacentValues(array, "*", `${product}`);
-            console.log(array)
-        } else if (item === "/" && (array.includes("*") !== true)) {
+        if (item === "/") {
             let denominator = findAdjacentValues(array, item)[1];
             notZero(denominator);
             let quotient = division(findAdjacentValues(array, item));
             replaceOperationAndAdjacentValues(array, "/", `${quotient}`);
+        } else if (item === "*" && (array.includes("/") !== true)) {
+            let product = multiplication(findAdjacentValues(array, item));
+            replaceOperationAndAdjacentValues(array, "*", `${product}`);            
         } else if (item === "+" && (array.includes("*") !== true) && (array.includes("/") !== true)) {
             let sum = addition(findAdjacentValues(array, item));
             replaceOperationAndAdjacentValues(array, "+", `${sum}`);
@@ -123,3 +120,17 @@ function clearAll() {
     currentOperation = [];
 }
 
+function findAdjacentValues(array, value) {
+    let left = array.indexOf(value) - 1;
+    let right = array.indexOf(value) + 1;
+    let result = [parseFloat(array[left]), parseFloat(array[right])];
+    return result;
+};
+
+function replaceOperationAndAdjacentValues(array, operation, result) {
+    let left = array.indexOf(operation) - 1;
+    array.splice(left, 3, result);
+};
+
+//add plus/minus button that toggles button values to be positive or negative
+//add decimals
